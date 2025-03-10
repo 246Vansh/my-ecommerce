eaecb6e526fc77c1ca3b7fa027b14e7299a8eef5
246Vansh <vanshdefender@gmail.com>
2025-03-10 14:39:32 +0530

    make main content js and dynamic para generate js/index.js

--- js/index.js
+++ js/index.js
@@ -50,3 +50,31 @@ document.addEventListener("click", (event) => {
         cookieBanner.remove();
     }
 });
+
+
+const featureParagraph = `
+We're committed to responsible, sustainable, and ethical manufacturing.
+Our small-scale approach allows us to focus on quality and reduce our impact.
+We're doing our best to delay the inevitable heat-death of the universe.`;
+
+let index = 0;
+
+function generateText() {
+    if (index < featureParagraph.length) {
+        const featurePara = document.querySelector(".feature-para");
+        const span = document.createElement("span");
+        span.innerText = featureParagraph[index];
+        span.style.opacity = "0";
+        span.style.transition = "opacity 0.3s ease-in-out";
+        
+        featurePara.appendChild(span);
+        setTimeout(() => span.style.opacity = "1", 50);
+        
+        index++;
+        setTimeout(generateText, featureParagraph[index] === " " ? 100 : 50);
+    }
+}
+window.onload = function () {
+    document.querySelector(".feature-para").innerText = "";
+    generateText();
+};

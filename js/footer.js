0532e624dcabbb3b795ed0020b4de9300f549014
246Vansh <vanshdefender@gmail.com>
2025-04-24 21:53:13 +0530

    make a logout user funcnality

--- js/footer.js
+++ js/footer.js
@@ -3,6 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-placeholder").innerHTML = data;
+            accountLogout();
         })
         .catch(error => console.error("Error loading footer:", error));
 });
+
+
+function accountLogout() {
+    const btn = document.getElementById("logOut");
+    btn.addEventListener("click", () => {
+        const data = localStorage.getItem("userInfo");
+        if (data) {
+            localStorage.removeItem("userInfo")
+            alert("✅Your Are logged Out")
+            location.reload()
+        } else {
+            alert("❌First Please Login Or Create Account")
+        }
+    })
+}
